e5121b1e59ee6b35dc96ccc3c3d41ee742b70b4b
Kyle Napod <knnapod@puc.edu>
2015-09-30 20:49:39 -0700

    Started Function
    Started working on mini functions for  merge.

--- functions.c
+++ functions.c
@@ -63,5 +63,68 @@ void bubble(int length, int a[])
 	return;
 }
 
+void mSort(int a, int low, int mid, int high)
+{
+	int i, j, k, l, temp;
+	
+	i=low;
+	j=mid+l;
+	l=low;
 
+	while((l<=mid)&&(j<=high))
+	{
+		if(a[l]<=a[j])
+		{
+		   temp[i]=a[l];
+	       l++;
+		}
+	    else
+		{
+	       temp[i]=arr[j];
+	       j++;
+	    }
+	    i++;
+	}
+
+	if(l>mid)
+	{
+		for(k=j;k<=high;k++)
+	   {
+		   temp[i]=a[k];
+		   i++;
+	   }
+	}
+	else
+	{
+		for(k=l;k<=mid;k++)
+		{
+			temp[i]=a[k];
+			i++;
+		}
+	}
+   
+	for(k=low;k<=high;k++)
+		{
+			a[k]=temp[k];
+		}
+}
+
+void partition(int a[], int low, int high)
+{
+	int mid;
+	
+	if(low<high)
+	{
+		mid = (low+high)/2;
+		partition(a,low,mid);
+		partition(a,mid+1,high);
+		mSort(a,low,mid,high);
+	}
+	return;
+}
+
+void merge(int length, int a[])
+{
+	partition(a, 0, length-1);
+}
 
